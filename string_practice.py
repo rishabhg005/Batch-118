af71e7b4c14e47d2c979ca8fad7e75bf92b9a1a8
rishabhg005 <rishabhg005@gmail.com>
2026-06-22 14:16:24 +0530

    new

---
+++ string_practice.py
@@ -0,0 +1,57 @@
+# str1="THis is python code in vs"
+# print(str1)
+# str1.remove(vs)
+# name="rishabh"
+# print(name)
+# str2="my name is rishabh"
+# str2.remove(rishabh")
+# print(str2)
+
+
+# waf to remove a word from a string
+# str1="this is python code in vs"
+# res=str1.removesuffix("vs")
+# print(res)
+
+
+#waf to remove words if vowel present from string
+# str1="this is python code in vs"
+# res=str1.split()
+# for i in res:
+#     if "aeiouAEIOU" not in i:
+#         final= " ".join(i)
+# print(final)
+
+
+
+    
+    
+
+    
+
+
+
+#waf to count total whitespace in a string
+# str1="this is python code in vs"
+# res=str1.count(" ")
+# print(res)
+
+# waf to remove all the whitespace from a string
+# str1="this is python code in vs"
+# res=str1.replace(" ","")
+# print(res)
+#waf to print all the words containes string number.
+str1="this is python code123 in vs version-35 address noida-11096"
+res=str1.split()
+for i in res:
+    print(i)
+  
+
+# waf to reverse all words fo stirng.
+# str1="this is python code in vs"
+# res=str1.split()
+# final=res[::-1]
+# print(final)
+
+# waf to extract only numbers for string
+# str1="this is python code123 in vs version-35 address noida-11096"
